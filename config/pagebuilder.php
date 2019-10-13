5f7f6aa6744a68af9dca875396ba20d610b73975
Hans Schouten <hans@falcosolutions.nl>
2019-10-13 11:30:47 +0200

    Initial config added

---
+++ config/pagebuilder.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | Themes settings
+     |--------------------------------------------------------------------------
+     |
+     | Pagebuilder requires a themes folder in which for each theme the individual
+     | theme blocks are defined.
+     |
+     */
+    'themes' => [
+        'path'       => resource_path('themes'),
+    ],
+];
