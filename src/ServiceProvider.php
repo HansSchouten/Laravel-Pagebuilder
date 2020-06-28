9b1c98f3ad55c7ab117566d667fec3bca166a34e
Hans Schouten <hans@falcosolutions.nl>
2020-06-28 08:49:51 +0200

    Register PHPageBuilder as singleton

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,6 +5,8 @@ namespace HansSchouten\LaravelPageBuilder;
 use HansSchouten\LaravelPageBuilder\Commands\CreateTheme;
 use HansSchouten\LaravelPageBuilder\Commands\PublishDemo;
 use HansSchouten\LaravelPageBuilder\Commands\PublishTheme;
+use PHPageBuilder\PHPageBuilder;
+use Exception;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
@@ -21,6 +23,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      * Bootstrap services.
      *
      * @return void
+     * @throws Exception
      */
     public function boot()
     {
@@ -35,10 +38,15 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
             ]);
         }
 
+        // register singleton phppagebuilder (this ensures phpb_ helpers have the right config without first manually creating a PHPageBuilder instance)
+        $this->app->singleton('phpPageBuilder', function($app) {
+            return new PHPageBuilder(config('pagebuilder'));
+        });
+        $this->app->make('phpPageBuilder');
+
         $this->publishes([
             __DIR__ . '/../config/pagebuilder.php' => config_path('pagebuilder.php'),
         ], 'config');
-
         $this->publishes([
             __DIR__ . '/../themes/demo' => base_path(config('pagebuilder.theme.folder_url') . '/demo'),
         ], 'demo-theme');
