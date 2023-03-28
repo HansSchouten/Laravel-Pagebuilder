c507c47323844c19d7a71624f8c8f260f828ee30
Chuman Shrestha <52157369+minuschuman@users.noreply.github.com>
2023-03-28 21:16:23 +0545

    Update boot logic for migration
    boot the configuration only after settings table is created show that error while running migration will be omitted

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace HansSchouten\LaravelPageBuilder;
 
+use Illuminate\Support\Facades\Schema;
 use HansSchouten\LaravelPageBuilder\Commands\CreateTheme;
 use HansSchouten\LaravelPageBuilder\Commands\PublishDemo;
 use HansSchouten\LaravelPageBuilder\Commands\PublishTheme;
@@ -29,28 +30,30 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         $this->loadMigrationsFrom(__DIR__ . '/../migrations');
+        
+        if (Schema::hasTable('settings')) {
+            if ($this->app->runningInConsole()) {
+                $this->commands([
+                    CreateTheme::class,
+                    PublishTheme::class,
+                    PublishDemo::class,
+                ]);
+            } elseif (empty(config('pagebuilder'))) {
+                throw new Exception("No PHPageBuilder config found, please run: php artisan vendor:publish --provider=\"HansSchouten\LaravelPageBuilder\ServiceProvider\" --tag=config");
+            }
 
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                CreateTheme::class,
-                PublishTheme::class,
-                PublishDemo::class,
-            ]);
-        } elseif (empty(config('pagebuilder'))) {
-            throw new Exception("No PHPageBuilder config found, please run: php artisan vendor:publish --provider=\"HansSchouten\LaravelPageBuilder\ServiceProvider\" --tag=config");
+            // register singleton phpPageBuilder (this ensures phpb_ helpers have the right config without first manually creating a PHPageBuilder instance)
+            $this->app->singleton('phpPageBuilder', function($app) {
+                return new PHPageBuilder(config('pagebuilder') ?? []);
+            });
+            $this->app->make('phpPageBuilder');
+
+            $this->publishes([
+                __DIR__ . '/../config/pagebuilder.php' => config_path('pagebuilder.php'),
+            ], 'config');
+            $this->publishes([
+                __DIR__ . '/../themes/demo' => base_path(config('pagebuilder.theme.folder_url') . '/demo'),
+            ], 'demo-theme');
         }
-
-        // register singleton phpPageBuilder (this ensures phpb_ helpers have the right config without first manually creating a PHPageBuilder instance)
-        $this->app->singleton('phpPageBuilder', function($app) {
-            return new PHPageBuilder(config('pagebuilder') ?? []);
-        });
-        $this->app->make('phpPageBuilder');
-
-        $this->publishes([
-            __DIR__ . '/../config/pagebuilder.php' => config_path('pagebuilder.php'),
-        ], 'config');
-        $this->publishes([
-            __DIR__ . '/../themes/demo' => base_path(config('pagebuilder.theme.folder_url') . '/demo'),
-        ], 'demo-theme');
     }
 }
